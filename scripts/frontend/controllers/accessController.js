270b5821ee612177c7872520212ab18c1ecd102e
Erick Dominguez <erick.dominguez@autozone.com>
2018-08-01 17:13:45 -0600

    access button must be pressed at leat 2 seconds to open a door

--- scripts/frontend/controllers/accessController.js
+++ scripts/frontend/controllers/accessController.js
@@ -11,12 +11,14 @@ angular.module('app').controller("accessController", ['$scope', '$http', 'wsClie
 
 
         $scope.activate = function (device) {
-            $http.post("/api/access/activate", device)
+            if($scope.timeDiff > 2){
+                $http.post("/api/access/activate", device)
                 .then(function (res) {
                     console.log(res);
                 }).catch(function (err) {
                     console.log(err);
                 });
+            }           
         }
 
         //this function is called every time a websocket message is received with type WebSocketsMessageType.DEVICE_DATA
@@ -26,6 +28,16 @@ angular.module('app').controller("accessController", ['$scope', '$http', 'wsClie
                 $scope.$apply();
             
         });
+
+        $scope.mouseDown = function () {
+            timeStart = new Date();
+        }
+
+        $scope.mouseUp = function () {
+            timeEnd = new Date();
+            $scope.timeDiff = (timeEnd - timeStart)/1000;
+            console.log($scope.timeDiff);
+        }  
 
 
     }
